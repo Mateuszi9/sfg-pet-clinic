b14bbafb4652b60c75381bc7aa99e300b7979059
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-06 12:44:53 +0100

    Add PetTypes in with Bootstrap (startup). Closes #32

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.example.sfgpetclinic.bootstrap;
 
 import com.example.sfgpetclinic.model.Owner;
+import com.example.sfgpetclinic.model.PetType;
 import com.example.sfgpetclinic.model.Vet;
 import com.example.sfgpetclinic.services.OwnerService;
+import com.example.sfgpetclinic.services.PetTypeService;
 import com.example.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,30 +15,38 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Owner owner1 = new Owner();
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        Owner owner1 = new Owner();
         owner1.setFirstName("Micahel");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
-
         owner3.setFirstName("Micahel3");
         owner3.setLastName("Weston3");
 
