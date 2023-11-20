96760d1e283b0c017ab8303458d1979405f132fb
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-20 10:28:03 +0100

    Write CRUD Tests for Owner Spring Data JPA Service. Closes #50

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -55,6 +55,7 @@ public class OwnerSDJpaService implements OwnerService {
 
     @Override
     public Owner findByLastName(String lastName) {
+
         return ownerRepository.findByLastName(lastName);
     }
 }
